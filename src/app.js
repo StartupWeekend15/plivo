12d1a8df174685430cf0b5743eb4098e72a427ce
Scott <scott@sscotth.io>
2015-04-11 18:23:38 -0500

    optional port ENV variable

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.post('/', function (req, res) {
   });
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address,
       port = server.address().port;
 
